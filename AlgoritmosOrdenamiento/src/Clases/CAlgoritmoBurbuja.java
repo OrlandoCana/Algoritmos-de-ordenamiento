db59ce43dc607e6202d3917238e4e4691a5fc598
[OrlandoCana] <[160888@unsaac.edu.pe]>
2021-12-03 03:05:12 -0500

    Interfaz Algoritmo - Clase Burbuja
    se implementa una interfaz IAlgoritmoOrdenamiento y una clase CAlgoritmoBurbuja

---
+++ AlgoritmosOrdenamiento/src/Clases/CAlgoritmoBurbuja.java
@@ -0,0 +1,67 @@
+package Clases;
+/**
+ * @author OrlandoCana
+ * @version 1.0
+ */
+public class CAlgoritmoBurbuja implements IAlgoritmoOrdenamiento{
+    private int[] Arreglo;
+    private int tamanio;
+    /**
+     * @param arreglo contiene numeros enteros
+     */
+    public CAlgoritmoBurbuja(int[] arreglo) {
+        this.Arreglo = arreglo;
+        this.tamanio = arreglo.length;
+    }
+    
+    public int[] getArreglo() {
+        return Arreglo;
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+    
+    @Override
+    public void Ordenar(boolean orden) {
+        int i = 0;
+        boolean valid = false;
+        while (i < this.tamanio && valid == false){
+            i += 1;
+            valid = true;
+            for (int j = 0; j < this.tamanio - i; j++){
+                if(orden == false){
+                    if(this.Arreglo[j] > this.Arreglo[j+1]){
+                        valid = false;
+                        // Intercambiar elemento j con el elemento j+1
+                        int aux = this.Arreglo[j];
+                        this.Arreglo[j] = this.Arreglo[j+1];
+                        this.Arreglo[j+1] = aux;
+                    }
+                }
+                else{
+                    if(this.Arreglo[j] < this.Arreglo[j+1]){
+                        valid = false;
+                        // Intercambiar elemento j con el elemento j+1
+                        int aux = this.Arreglo[j];
+                        this.Arreglo[j] = this.Arreglo[j+1];
+                        this.Arreglo[j+1] = aux;
+                    }
+                }
+            }
+        }
+    }
+    @Override
+    public void Ordenar() {
+        this.Ordenar(false);
+    }
+    @Override
+    public void MostrarArreglo() {
+        for (int k = 0; k < this.tamanio; k++){
+            System.out.print(this.Arreglo[k] + " ");
+        }
+        System.out.println();
+    }
+    
+    
+}
---
+++ AlgoritmosOrdenamiento/src/Clases/IAlgoritmoOrdenamiento.java
@@ -0,0 +1,10 @@
+package Clases;
+/**
+ * @author OrlandoCana
+ * @version 1.0
+ */
+public interface IAlgoritmoOrdenamiento {
+    void Ordenar();
+    void Ordenar(boolean orden);
+    void MostrarArreglo();
+}
